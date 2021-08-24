b8444138f93273a843e317a469a07f044c33ba71
Jordan <Jordanabid@gmail.com>
2021-08-24 17:17:34 +0200

    Formateur presque ok

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import {RouterModule, Routes} from '@angular/router';
 import {EvaluationComponent} from "./evaluation/evaluation.component";
 import {AccueilComponent} from "./accueil/accueil.component";
 import {SalleComponent} from "./salle/salle.component";
+import {FormateurComponent} from "./formateur/formateur.component";
 
 const routes: Routes = [
   {path: "accueil", component: AccueilComponent},
   {path: "evaluation", component: EvaluationComponent},
   {path : "salle", component: SalleComponent},
+  {path : "formateur", component: FormateurComponent},
   {path: "", redirectTo: "accueil", pathMatch: "full"}
 ];
 
