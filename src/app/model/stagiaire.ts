128976326346e50cce8d7b365223448415a84bfd
Jordan <Jordanabid@gmail.com>
2021-08-24 16:46:31 +0200

    maj adresse into stagiaire

--- src/app/model/stagiaire.ts
+++ src/app/model/stagiaire.ts
@@ -1,11 +1,12 @@
 import {Personne} from "./personne";
+import {Adresse} from "./adresse";
 
 export class Stagiaire extends Personne{
   dtNaissance:Date;
   niveauEtude:string;
 
-  constructor(id?:number,version?:number,civilite?:string,nom?:string,prenom?:string,email?:string,telephone?:string,dtNaissance?:Date,niveauEtude:string) {
-    super(id,version,civilite,nom,prenom,email,telephone);
+  constructor(id?:number,version?:number,civilite?:string,nom?:string,prenom?:string,email?:string,telephone?:string,dtNaissance?:Date,niveauEtude?:string,adresse?:Adresse) {
+    super(id,version,civilite,nom,prenom,email,telephone,adresse);
     this.dtNaissance=dtNaissance;
     this.niveauEtude=niveauEtude;
   }
